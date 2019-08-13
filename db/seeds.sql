95ce8d4ff752e3c5cee08243eaa6de6a4dc90c72
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-08-13 09:57:58 -0400

    working program

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Apple Butter Burger');
+INSERT INTO burgers (burger_name) VALUES ('Red Pepper Burger');
+INSERT INTO burgers (burger_name) VALUES ('Pepperjack Burger');
